d8518c84cc27dca98aa24acc2539d052ca0990f4
Marcio Viana Cacovichi <112443172+Mvcacovichi@users.noreply.github.com>
2024-08-13 21:49:19 -0300

    Add files via upload

---
+++ emojipedia.js
@@ -0,0 +1,26 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "🙏️",
+      name: "Pessoa orando com mãos palma com palma",
+      meaning:
+        "Duas mãos cerradas. Acaba de voltar a si, faz uma prece ou almeja a iluminação. Também usado para “bate-aqui“, bem como “obrigado“."
+    },
+    {
+      id: 2,
+      emoji: "🤩️",
+      name: "Carinha sorridente com olhos de estrela",
+      meaning:
+        "Está muito impressionado e totalmente animado com algo. Surpreso e admirado depois de encontrar alguém de quem se gosta muito. Fascinado com a fama e o universo das celebridades."
+    },
+    {
+      id: 3,
+      emoji: "✊️",
+      name: "Punho levantado",
+      meaning:
+        "Eu vou conseguir! O punho levantado implica poder e força. Representa algo em que você acredita. Bem como, gesto político e sinal de teimosia, solidariedade e resistência."
+    }
+  ];
+  
+
+  export default emojipedia;
